b5cce97ecdea859702730c45007e2e7d50673e3e
yoonpyo <yphong@hyunees.com>
2021-08-01 18:42:37 +0900

    Interface on typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-const sayHi = (name: string, age: number, gender: string) : string => {
-    return `Hi, ${name}! you are ${age}, you are a ${gender}`
+interface Human {
+    name: string,
+    age: number,
+    gender: string
 }
 
-console.log(sayHi("yoon pyo", 32, "male"));
+const person = {
+    name: "yoon pyo",
+    age: 32,
+    gender: "male"
+}
+
+const sayHi = (person: Human) : string => {
+    return `Hi, ${person.name}! you are ${person.age}, you are a ${person.gender}`
+}
+
+console.log(sayHi(person));
 
 export {}
