bf9821e92db9d520f1372f72ad770dcd76a20e27
yoonpyo <yphong@hyunees.com>
2021-08-01 18:21:53 +0900

    add files

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const sayHi = (name: string, age: number, gender: string) : string => {
+    return `Hi, ${name}! you are ${age}, you are a ${gender}`
+}
+
+console.log(sayHi("yoon pyo", 32, "male"));
+
+export {}
